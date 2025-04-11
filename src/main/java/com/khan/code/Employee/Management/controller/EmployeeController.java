2478e76edad577e5bb1ca9bf6d412404c7714156
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-11 13:49:30 +0200

    added EmployeeController calss

---
+++ src/main/java/com/khan/code/Employee/Management/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.khan.code.Employee.Management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
