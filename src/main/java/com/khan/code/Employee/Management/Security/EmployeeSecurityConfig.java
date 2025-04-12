f50a1a46720e334cff3013276b98ad236dbf1dce
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-12 15:11:32 +0200

    implemented custom restricted URL feature

--- src/main/java/com/khan/code/Employee/Management/Security/EmployeeSecurityConfig.java
+++ src/main/java/com/khan/code/Employee/Management/Security/EmployeeSecurityConfig.java
@@ -1,35 +1,61 @@
 package com.khan.code.Employee.Management.Security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class EmployeeSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-
-        UserDetails abdul = User.builder()
-                .username("abdul")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+    public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails john = User.builder()
                 .username("john")
                 .password("{noop}test123")
-                .roles("EMPLOYEE, MANAGER")
+                .roles("EMPLOYEE")
                 .build();
 
-        UserDetails mark = User.builder()
-                .username("mark")
+        UserDetails mary = User.builder()
+                .username("mary")
                 .password("{noop}test123")
-                .roles("EMPLOYEE, MANAGER","ADMIN")
+                .roles("EMPLOYEE", "MANAGER")
                 .build();
 
-        return new InMemoryUserDetailsManager(abdul, john, mark);
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                ).exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+
 }
--- src/main/java/com/khan/code/Employee/Management/controller/EmployeeController.java
+++ src/main/java/com/khan/code/Employee/Management/controller/EmployeeController.java
@@ -6,8 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index() {
         return "index";
     }
+
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+
+        return "systems";
+    }
+
 }
